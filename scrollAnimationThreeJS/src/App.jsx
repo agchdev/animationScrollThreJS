130deb13c938db685371fe59e5789fb5f706b839
unknown <alex.chinchi02@gmail.com>
2024-11-18 12:56:05 +0100

    cambios

--- scrollAnimationThreeJS/src/App.jsx
+++ scrollAnimationThreeJS/src/App.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react'
 import './App.css'
 import Scene from './components/Scene'
+import Labels from './components/Labels'
 
 function App() {
 
   return (
     <>
       <div className='scene_container'>
+        <Labels />
         <Suspense fallback={null}>
           <Scene/>  
         </Suspense>  
